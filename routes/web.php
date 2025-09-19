61a0a97312c190ac0a4cbc12dc0dc5116d2f31e0
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-09-19 21:32:33 +0330

    make route for my experience index page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\backend\AdminController;
 use App\Http\Controllers\backend\HeroController;
+use App\Http\Controllers\backend\ResumeController;
 use App\Http\Controllers\backend\ServicesController;
 use App\Http\Controllers\frontend\FrontendController;
 use App\Http\Controllers\ProfileController;
@@ -13,22 +14,36 @@ Route::middleware('auth')->group(function(){
     Route::get('/dashboard', function () {
         return view('admin.pages.index');
     })->middleware(['verified'])->name('dashboard');
+    // admin basic routes
     Route::get('/admin/logout' , [AdminController::class , 'logout'])->name('admin.logout');
     Route::get('/admin/edit/profile' , [AdminController::class , 'profileEdit'])->name('admin.edit.profile');
     Route::post('/admin/Update/profile' , [AdminController::class , 'profileUpdate'])->name('admin.update.profile');
     Route::get('/admin/Change/profile-Password' , [AdminController::class , 'profilePasswordChange'])->name('admin.change.password');
     Route::post('/admin/Change/profile-Password' , [AdminController::class , 'profilePasswordUpdate'])->name('admin.update.password');
+    // end admin basic routes
 
+    // hero section routes
     Route::get('hero-section' ,  [HeroController::class , 'heroSection'])->name('hero.section');
     Route::post('update-hero-section' ,  [HeroController::class , 'updateHeroSection'])->name('update.hero.section');
+    // end hero section routes
 
+    // services route
     Route::get('all-services' ,  [ServicesController::class , 'allServices'])->name('all.services');
     Route::get('add-service' ,  [ServicesController::class , 'addService'])->name('add.service');
     Route::post('store-service' ,  [ServicesController::class , 'storeService'])->name('store.service');
     Route::get('edit-service/{id}' ,  [ServicesController::class , 'editService'])->name('edit.service');
     Route::post('update-service' ,  [ServicesController::class , 'updateService'])->name('update.service');
     Route::get('delete-service/{id}' ,  [ServicesController::class , 'deleteService'])->name('delete.service');
-    
+    // end services route
+
+    // my experience routes
+    Route::get('my-experience' ,  [ResumeController::class , 'myExperience'])->name('my.experience');
+    // Route::get('add-experience' ,  [ResumeController::class , 'addService'])->name('add.service');
+    // Route::post('store-service' ,  [ResumeController::class , 'storeService'])->name('store.service');
+    // Route::get('edit-service/{id}' ,  [ResumeController::class , 'editService'])->name('edit.service');
+    // Route::post('update-service' ,  [ResumeController::class , 'updateService'])->name('update.service');
+    // Route::get('delete-service/{id}' ,  [ResumeController::class , 'deleteService'])->name('delete.service');
+
 });
 
 
