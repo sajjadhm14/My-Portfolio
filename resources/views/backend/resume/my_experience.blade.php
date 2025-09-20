358404e42629fca98275b528d52037a5fc50731c
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-09-20 12:54:56 +0330

    make modal table in my experience blade view

--- resources/views/backend/resume/my_experience.blade.php
+++ resources/views/backend/resume/my_experience.blade.php
@@ -1,6 +1,14 @@
 @extends('admin.admin_dashboard')
 
 @section('admin')
+
+
+
+<nav class="page-breadcrumb">
+					<ol class="breadcrumb">
+						<a href="" class="btn btn-inverse-warning" data-bs-toggle="modal" data-bs-target="#varyingModal">Add Experience</a>
+					</ol>
+				</nav>
 <div class="row">
 					<div class="col-md-12 grid-margin stretch-card">
             <div class="card">
@@ -42,4 +50,44 @@
             </div>
 					</div>
 				</div>
+        {{-- add experience model --}}
+         <div class="modal fade" id="varyingModal" tabindex="-1" aria-labelledby="varyingModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="varyingModalLabel">Add Experience </h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form>
+                        <input type="hidden" name="resume_cat" value="experience">
+                        <div class="mb-3">
+                          <label for="title" class="form-label">Title</label>
+                          <input type="text" class="form-control" name = "resume_title" placeholder="Lead Developer" required>
+                        </div>
+
+                        <div class="mb-3">
+                          <label for="organization" class="form-label">Organization</label>
+                          <input type="text" class="form-control" name = "institution" placeholder="London" required>
+                        </div>
+
+                        <div class="mb-3">
+                          <label for="from" class="form-label">From Year</label>
+                          <input type="text" class="form-control" name = "from_year" placeholder="2022" required>
+                        </div>
+                        <div class="mb-3">
+                          <label for="to" class="form-label">To Year</label>
+                          <input type="text" class="form-control" name = "to_year" placeholder="present" required>
+                        </div>
+
+                         <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                          <button type="sub,it" class="btn btn-primary">Add Experience</button>
+                         </div>
+                      </form>
+                    </div>
+                   
+                  </div>
+                </div>
+              </div>
 @endsection
