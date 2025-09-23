3416c46cfb86591c0968ac9cc498a2c15de08b12
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-09-23 09:50:03 +0330

    modify skills.blade file and add delete button route

--- resources/views/frontend/partials/skills.blade.php
+++ resources/views/frontend/partials/skills.blade.php
@@ -11,64 +11,29 @@
                   </div>
                </div>
             </div>
-
+            @php
+                $skills = App\Models\Skill::all();
+                necessary
+            @endphp
             <div class="row">
                <div class="col-md-12">
                   <div class="skills-widget d-flex flex-wrap justify-content-center align-items-center">
-                     <div class="skill-item wow fadeInUp" data-wow-delay=".3s">
-                        <div class="skill-inner">
-                           <div class="icon-box">
-                              <img src="{{asset('frontend/assets/img/icons/php-svgrepo-com.svg')}}" alt="" />
+                     @unless (count($skills) == 0)
+                       @foreach ($skills as $skill)
+                           <div class="skill-item wow fadeInUp" data-wow-delay=".3s">
+                              <div class="skill-inner">
+                                 <div class="icon-box">
+                                    <img src="{{asset($skill->icon)}}" alt="" />
+                                 </div>
+                              <div class="number">{{$skill->exp_level}}</div>
                            </div>
-                           <div class="number">92%</div>
-                        </div>
-                        <p>PHP</p>
-                     </div>
-                     <div class="skill-item wow fadeInUp" data-wow-delay=".4s">
-                        <div class="skill-inner">
-                           <div class="icon-box">
-                              <img src="{{asset('frontend/assets/img/icons/mysql-logo-svgrepo-com.svg')}}" alt="" />
-                           </div>
-                           <div class="number">80%</div>
-                        </div>
-                        <p>MySQl</p>
-                     </div>
-                     <div class="skill-item wow fadeInUp" data-wow-delay=".5s">
-                        <div class="skill-inner">
-                           <div class="icon-box">
-                              <img src="{{asset('frontend/assets/img/icons/laravel-2.svg')}}" alt="" />
-                           </div>
-                           <div class="number">85%</div>
-                        </div>
-                        <p>Laravel</p>
-                     </div>
-                     <div class="skill-item wow fadeInUp" data-wow-delay=".6s">
-                        <div class="skill-inner">
-                           <div class="icon-box">
-                              <img src="{{asset('frontend/assets/img/icons/livewire.svg')}}" alt="" />
-                           </div>
-                           <div class="number">99%</div>
-                        </div>
-                        <p>LiveWire</p>
-                     </div>
-                     <div class="skill-item wow fadeInUp" data-wow-delay=".7s">
-                        <div class="skill-inner">
-                           <div class="icon-box">
-                              <img src="{{asset('frontend/assets/img/icons/github-svgrepo-com.svg')}}" alt="" />
-                           </div>
-                           <div class="number">89%</div>
-                        </div>
-                        <p>React</p>
-                     </div>
-                     <div class="skill-item wow fadeInUp" data-wow-delay=".8s">
-                        <div class="skill-inner">
-                           <div class="icon-box">
-                              <img src="{{asset('frontend/assets/img/icons/js.svg')}}" alt="" />
-                           </div>
-                           <div class="number">93%</div>
-                        </div>
-                        <p>JavaScript</p>
-                     </div>
+                        <p>{{$skill->technology_name}}</p>
+                     </div>    
+                       @endforeach
+                     @else
+                        <p>No Skill Found !</p>
+                     @endunless
+                    
                   </div>
                </div>
             </div>
