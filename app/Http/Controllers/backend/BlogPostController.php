ccaeed25f34df2adbb2cb25ba573bd494e3f05d2
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-09-24 14:56:05 +0330

    make all needed method

--- app/Http/Controllers/backend/BlogPostController.php
+++ app/Http/Controllers/backend/BlogPostController.php
@@ -27,10 +27,9 @@ class BlogPostController extends Controller
         $imagePath = 'uploads/blog/'.$imageName;
 
         $post = new BlogPost();
-
         $post->user_id = Auth::user()->id;
         $post->post_title = $request->post_title;
-        $post->post_slug = strtolower(str_replace(' ', '-', $request->post_slug));
+        $post->post_slug = strtolower(str_replace(' ', '-', $request->post_title));
         $post->photo = $imagePath;
         $post->post_tags = $request ->post_tags;
         $post->post_description = $request ->post_description;
@@ -40,6 +39,59 @@ class BlogPostController extends Controller
             'message' => 'Blog Posted Successfully!',
             'alert-type' => 'success',
         ];
-        return redirect()->back()->with($notification);
+        return redirect()->route('all.post')->with($notification);
+    }
+
+    public function allPost()
+    {
+        $posts = BlogPost::latest()->get();
+        return view('backend.blog.all_posts' , compact('posts'));
+    }
+    public function editPost($id)
+    {
+        $post = BlogPost::findOrFail($id);
+        return view('backend.blog.edit_post' , compact('post'));
+    }
+
+    public function updatePost(Request $request) 
+    {
+        if($request->hasFile('post_photo')){
+            $file = $request->file('post_photo');
+            $imageName = 'post_'.hexdec(uniqid()).'.'.$file->getClientOriginalExtension();
+            $manager = new ImageManager(new Driver);
+            $img = $manager->read($file);
+            $img = $img->resize(409,368);
+            $img = $img->toJpeg(80)->save(base_path('public/uploads/blog/'.$imageName));
+            $imagePath = 'uploads/blog/'.$imageName;
+
+            $post =BlogPost::find($request->id);
+            $post->user_id = Auth::user()->id;
+            $post->post_title = $request->post_title;
+            $post->post_slug = strtolower(str_replace(' ', '-', $request->post_title));
+            $post->photo = $imagePath;
+            $post->post_tags = $request ->post_tags;
+            $post->post_description = $request ->post_description;
+            $post->save();
+
+            $notification = [
+                'message' => 'Blog Updated Successfully!',
+                'alert-type' => 'info',
+            ];
+            return redirect()->route('all.post')->with($notification);
+        }
+    }
+
+    public function deletePost($id)
+    {
+        $oldPostPhoto = BlogPost::find($id);
+        unlink($oldPostPhoto->photo);
+        
+
+        BlogPost::findOrFail($id)->delete();
+        $notification = [
+                'message' => 'Blog Deleted Successfully!',
+                'alert-type' => 'success',
+            ];
+            return redirect()->route('all.post')->with($notification);
     }
 }
