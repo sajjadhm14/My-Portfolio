2eb439c16b5b839ba8e579099e1086f1f35c31d5
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-09-25 10:31:54 +0330

    make blog detail method

--- app/Http/Controllers/frontend/FrontendController.php
+++ app/Http/Controllers/frontend/FrontendController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\BlogPost;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
@@ -10,5 +11,12 @@ class FrontendController extends Controller
     public function homepage()
     {
         return view ('frontend.homepage');
+    }
+
+    public function blogDetails($slug)
+    {
+        $post = BlogPost::where('post_slug' , $slug)->first();
+        $rPosts = BlogPost::latest()->limit(3)->get();
+        return view('frontend.blog.post_details', compact( 'post' , 'rPosts'));
     }
 }
