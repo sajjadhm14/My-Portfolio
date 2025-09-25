87dfca505522277f967b22c6a1d6af53e97f43ad
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-09-25 21:39:38 +0330

    make store comment in frontend controller

--- app/Http/Controllers/frontend/FrontendController.php
+++ app/Http/Controllers/frontend/FrontendController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use App\Models\BlogPost;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class FrontendController extends Controller
 {
@@ -18,5 +19,20 @@ class FrontendController extends Controller
         $post = BlogPost::where('post_slug' , $slug)->first();
         $rPosts = BlogPost::latest()->limit(3)->get();
         return view('frontend.blog.post_details', compact( 'post' , 'rPosts'));
+    }
+
+    public function storeComment(Request $request)
+    {
+        $comment = new Comment();
+        $comment ->post_id = $request->post_id;
+        $comment ->user_name = $request->user_name;
+        $comment ->user_email = $request->user_email;
+        $comment ->comment = $request->comment;
+        $comment->save();
+        $notification = [
+                'message' => 'Comment Posted! Waiting For Approval',
+                'alert-type' => 'success',
+            ];
+            return redirect()->back()->with($notification);
     }
 }
