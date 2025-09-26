a7c0a2eb119710399598651556d5be7fa05cb164
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-09-26 11:49:25 +0330

    make migration field for comments table

---
+++ database/migrations/2025_09_25_075514_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('post_id');
+            $table->string('user_name');
+            $table->string('user_email');
+            $table->text('comment');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
