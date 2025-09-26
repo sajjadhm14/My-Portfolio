a464341740645efbd169d18a3e6a072c40fc20c0
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-09-26 18:04:53 +0330

    change hompage items

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -4,7 +4,7 @@
    <meta charset="utf-8" />
    <meta http-equiv="x-ua-compatible" content="ie=edge" />
    <meta name="viewport" content="width=device-width, initial-scale=1" />
-   <meta name="description" content="Hey there! Im sajjad and welcome to my Portfolio website />
+   <meta name="description" content="Hey there! Im Sajjad and welcome to my Portfolio website />
 
    <!-- Site Title -->
    <title>Sajjad Hamidi </title>
