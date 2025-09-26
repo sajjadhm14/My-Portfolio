75bbae14235c254df96f4ced563485552f3686fe
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-09-26 16:48:57 +0330

    make header fully dynamic

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,16 +1,19 @@
+@php
+    $siteSettings = App\Models\SiteSetting::findOrFail(1);
+@endphp
 <header class="tj-header-area header-absolute">
       <div class="container">
          <div class="row">
             <div class="col-12 d-flex flex-wrap align-items-center">
                <div class="logo-box">
                   <a href="/">
-                     <img src="{{asset('frontend/assets/img/logo/logo.png')}}" alt="" />
+                     <img src="{{asset($siteSettings->logo)}}" alt="" />
                   </a>
                </div>
 
                <div class="header-info-list d-none d-md-inline-block">
                   <ul class="ul-reset">
-                     <li><a href="mailto:sajjadhamidi499@gmail.com">sajjadhamidi499@gmail.com</a></li>
+                     <li><a href="mailto:{{$siteSettings->email}}">{{$siteSettings->email}}</a></li>
                   </ul>
                </div>
                <div class="header-menu">
@@ -25,7 +28,7 @@
                   </nav>
                </div>
                <div class="header-button">
-                  <a href="/" class="btn tj-btn-primary">Hire me!</a>
+                  <a href="https://t.me/pharmoba" class="btn tj-btn-primary">Hire me!</a>
                </div>
                <div class="menu-bar d-lg-none">
                   <button>
@@ -45,13 +48,13 @@
             <div class="col-12 d-flex flex-wrap align-items-center">
                <div class="logo-box">
                   <a href="#">
-                     <img src="{{asset('frontend/assets/img/logo/logo.png')}}" alt="" />
+                     <img src="{{asset($siteSettings->logo)}}" alt="" />
                   </a>
                </div>
 
                <div class="header-info-list d-none d-md-inline-block">
                   <ul class="ul-reset">
-                     <li><a href="mailto:mustaphajnamadi@gmail.com">sajjadhamidi499@gmail.com</a></li>
+                     <li><a href="mailto:{{$siteSettings->email}}">{{$siteSettings->email}}</a></li>
                   </ul>
                </div>
 
@@ -69,7 +72,7 @@
                </div>
 
                <div class="header-button">
-                  <a href="#" class="btn tj-btn-primary">Hire me!</a>
+                  <a href="https://t.me/pharmoba" class="btn tj-btn-primary">Hire me!</a>
                </div>
 
                <div class="menu-bar d-lg-none">
